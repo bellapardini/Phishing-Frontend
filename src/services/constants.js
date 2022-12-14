c8b294815b1f1bd25059c9c44efc82c9eae46b7b
bellapardini <74802820+bellapardini@users.noreply.github.com>
2022-12-14 02:49:17 -0300

    Create constants.js

---
+++ src/services/constants.js
@@ -0,0 +1,6 @@
+// export const URL = 'http://localhost:9000/.netlify/functions/api';
+export const URL = 'https://heroic-llama-7852a9.netlify.app/.netlify/functions/api';
+
+export const SITE_NAME = 'Cyber Game';
+
+export const CHAR_NAMES = ['Vilma', 'Miguel'];
