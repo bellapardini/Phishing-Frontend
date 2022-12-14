934da68c03966484cc8f9ebe501b00715a939d90
bellapardini <74802820+bellapardini@users.noreply.github.com>
2022-12-14 02:48:39 -0300

    Create basicInfo.js

---
+++ src/services/basicInfo.js
@@ -0,0 +1,15 @@
+export const tools = [
+  'Internet',
+  'E-mail',
+  'Conta Bancária',
+  'Pacote Office',
+  'Redes Sociais',
+];
+
+export const basicTexts = [
+  'Websites são páginas virtuais que têm um endereço eletrônico acessível pela barra do navegador. As páginas mais seguras começam com https. Outra forma de encontrar um website é através de um buscador, como o Google.',
+  'O e-mail é uma forma de correspondência eletrônica e através dele você pode conversar com qualquer pessoa no mundo. Lembre-se de não compartilhar sua senha e tome cuidado com mensagens de desconhecidos.',
+  'É possível ganhar tempo fazendo suas atividades bancárias pela internet. Contudo, é preciso ser prudente. Tenha em mente que o banco nunca vai pedir seus dados fora da página deles.',
+  'No Microsoft Word, você pode escrever e editar textos. Com o Excel, você pode elaborar tabelas e fazer cálculos com mais facilidade. Já o Power-Point é uma ferramenta útil para preparar apresentações.',
+  'Além dos aplicativos de celular, você também pode acessar as redes sociais através de websites. Basta buscar pelo nome de sua rede favorita que você a encontrará.',
+];
